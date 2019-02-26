bcf6703a38775c0dfce7e260597bff495a1a14f0
herudi7@gmail.com <herudi7@gmail.com>
2019-02-26 11:03:47 +0700

    add cors

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ var express = require('express');
 var router = require('./routes/router');
 var app = express();
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+app.use(allowCrossDomain);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', router);
